a626822b6b67621708cfc3afc36987fb7314ddd0
FabianSaldierna <fsaldierna@banxico.org.mx>
2025-03-17 00:22:35 -0600

    added solving code

---
+++ server/src/schemas/typeDefs.ts
@@ -0,0 +1,54 @@
+import { gql } from 'graphql-tag';
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    _id: ID
+    bookId: String
+    authors: [String]!
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input UserInput {
+    username: String
+    email: String
+    password: String
+  }
+
+  input BookInput {
+    bookId: String
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    addUser(input: UserInput!): Auth
+    login(email: String!, password: String!): Auth
+    saveBook(input: BookInput!): User
+    removeBook(bookId: String!): User
+  }
+`;
+
+export default typeDefs;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,25 +1,49 @@
 import express from 'express';
 import path from 'node:path';
+import type { Request, Response } from 'express';
 import db from './config/connection.js';
-import routes from './routes/index.js';
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import { typeDefs, resolvers } from './schemas/index.js';
+import { authenticateToken } from './services/auth.js';
 import { fileURLToPath } from 'node:url';
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../client/dist')));
-}
-
-app.use(routes);
-
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
 });
+
+const startApolloServer = async () => {
+  await server.start();
+  await db();
+
+  const PORT = process.env.PORT || 3001;
+  const app = express();
+
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  app.use('/graphql', expressMiddleware(server as any,
+    {
+      context: authenticateToken as any
+    }
+  ));
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../../client/dist')));
+  
+    app.get('*', (_req: Request, res: Response) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
+  
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+
+};
+
+startApolloServer();
